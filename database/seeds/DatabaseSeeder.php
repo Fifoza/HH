5a681dc5afefb16d9166da7894f97bf3f18441a3
MartinPavlinovNikolov <Martin_Nikolov.89@abv.bg>
2019-03-16 04:19:05 +0200

    burth day is over

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,11 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         factory(User::class, 10)->create();
-        $images = ['hb1.jpeg', 'hb2.jpeg', 'hb3.jpeg',];
-        factory(Creativity::class)->create(['user_id' => 1, 'image' => $images[0], 'description' => 'Честит Рожден Ден!']);
-        factory(Creativity::class)->create(['user_id' => 2, 'image' => $images[1], 'description' => 'Честит Рожден Ден!']);
-        factory(Creativity::class)->create(['user_id' => 3, 'image' => $images[2], 'description' => 'Честит Рожден Ден!']);
-        //factory(Creativity::class, 10)->create();
+
+        factory(Creativity::class, 100)->create();
         // $this->call(UsersTableSeeder::class);
     }
 }
