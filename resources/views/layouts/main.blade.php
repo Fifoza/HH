393ac8a963d904662e325f120b85f4a508a4c9ce
MartinPavlinovNikolov <martin_nikolov.89@abv.bg>
2019-05-21 08:48:57 +0300

    main background fix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,9 +16,9 @@
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="{{ URL::asset('css/main/index.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('css/mi-config.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('css/mi.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/main/index.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('css/main/nav.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('css/main/header.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('css/main/footer.css') }}">
